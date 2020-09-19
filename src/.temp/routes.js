abf947891f6242e91843c0310b5044565da27d23
Sergio Ramos <sergio.ramos@circusmarketing.com>
2020-09-19 12:58:31 -0500

    Products dynamic

--- src/.temp/routes.js
+++ src/.temp/routes.js
@@ -1,35 +1,134 @@
-const c1 = () => import(/* webpackChunkName: "page--src--pages--nut-house-vue" */ "/Users/sergioramos/Development/Projects/fun-fruit/src/pages/NutHouse.vue")
-const c2 = () => import(/* webpackChunkName: "page--src--pages--detail-vue" */ "/Users/sergioramos/Development/Projects/fun-fruit/src/pages/Detail.vue")
-const c3 = () => import(/* webpackChunkName: "page--src--templates--category-vue" */ "/Users/sergioramos/Development/Projects/fun-fruit/src/templates/Category.vue")
-const c4 = () => import(/* webpackChunkName: "page--node-modules--gridsome--app--pages--404-vue" */ "/Users/sergioramos/Development/Projects/fun-fruit/node_modules/gridsome/app/pages/404.vue")
-const c5 = () => import(/* webpackChunkName: "page--src--pages--index-vue" */ "/Users/sergioramos/Development/Projects/fun-fruit/src/pages/Index.vue")
+const c1 = () => import(/* webpackChunkName: "page--src--templates--product-vue" */ "/Users/sergioramos/Development/Projects/fun-fruit/src/templates/Product.vue")
+const c2 = () => import(/* webpackChunkName: "page--src--templates--category-vue" */ "/Users/sergioramos/Development/Projects/fun-fruit/src/templates/Category.vue")
+const c3 = () => import(/* webpackChunkName: "page--node-modules--gridsome--app--pages--404-vue" */ "/Users/sergioramos/Development/Projects/fun-fruit/node_modules/gridsome/app/pages/404.vue")
+const c4 = () => import(/* webpackChunkName: "page--src--pages--index-vue" */ "/Users/sergioramos/Development/Projects/fun-fruit/src/pages/Index.vue")
 
 export default [
   {
-    path: "/nut-house/",
+    path: "/nut-house/snack-miguelito-spicy-fruit-mix/",
     component: c1
   },
   {
-    path: "/detail/",
-    component: c2
+    path: "/nut-house/snack-miguelito-spicy-trail-mix/",
+    component: c1
   },
   {
-    path: "/category/",
-    component: c3
+    path: "/nut-house/snack-spicy-fruit-mix-25g/",
+    component: c1
+  },
+  {
+    path: "/nut-house/snack-spicy-trail-mix-25g/",
+    component: c1
+  },
+  {
+    path: "/nut-house/snack-mix-cereza/",
+    component: c1
+  },
+  {
+    path: "/nut-house/snack-mix-kiwi/",
+    component: c1
+  },
+  {
+    path: "/nut-house/snack-mix-papaya/",
+    component: c1
+  },
+  {
+    path: "/nut-house/snack-mix-piña/",
+    component: c1
+  },
+  {
+    path: "/nut-house/spicy-fruit-mix/",
+    component: c1
+  },
+  {
+    path: "/nut-house/spicy-trail-mix/",
+    component: c1
+  },
+  {
+    path: "/nut-house/nuez-de-la-india-tostada-y-salada/",
+    component: c1
+  },
+  {
+    path: "/nut-house/nuez-de-la-india-con-chipotle/",
+    component: c1
+  },
+  {
+    path: "/nut-house/pistache-tostado-y-salado/",
+    component: c1
+  },
+  {
+    path: "/nut-house/nuez-de-la-india-1/",
+    component: c1
+  },
+  {
+    path: "/nut-house/nuez-de-la-india-chipotle/",
+    component: c1
+  },
+  {
+    path: "/nut-house/nuez-pecanera-mitades-con-chipotle/",
+    component: c1
+  },
+  {
+    path: "/nut-house/mezcla-de-frutos-y-nueces/",
+    component: c1
+  },
+  {
+    path: "/nut-house/mezcla-de-higo-y-nueces/",
+    component: c1
+  },
+  {
+    path: "/nut-house/nuez-de-la-india/",
+    component: c1
+  },
+  {
+    path: "/nut-house/nuez-pecanera-en-mitades/",
+    component: c1
+  },
+  {
+    path: "/nut-house/pistache-tostado/",
+    component: c1
+  },
+  {
+    path: "/nut-house/nueces-mixtas-tostadas/",
+    component: c1
+  },
+  {
+    path: "/nut-house/nuez-pecanera-chipotle/",
+    component: c1
+  },
+  {
+    path: "/nut-house/nuez-pecanera-mitades/",
+    component: c1
+  },
+  {
+    path: "/nut-house/mango-enchilado-1/",
+    component: c1
+  },
+  {
+    path: "/nut-house/mango-enchilado/",
+    component: c1
+  },
+  {
+    path: "/nut-house/almendra-entera/",
+    component: c1
+  },
+  {
+    path: "/category/:slug/",
+    component: c2
   },
   {
     name: "404",
     path: "/404/",
-    component: c4
+    component: c3
   },
   {
     name: "home",
     path: "/",
-    component: c5
+    component: c4
   },
   {
     name: "*",
     path: "*",
-    component: c4
+    component: c3
   }
 ]
