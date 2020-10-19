a245297a5eaf543a9d68f474c1dcae381923fef3
Sergio Ramos <sergio.ramos@circusmarketing.com>
2020-10-19 15:53:45 -0500

    Blog mobile

--- src/components/LayoutBlog.vue
+++ src/components/LayoutBlog.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
     section.blog
-        .flex.flex-row.justify-center.items-start.my-10
-            .flex.flex-col.justify-start.ml-5(class="w-2/3")
+        .flex.sm_flex-row.flex-col.justify-center.items-start.my-10
+            .flex.flex-col.justify-start.sm_ml-5.mx-3(class="sm_w-2/3")
                 h2.text-ui-gray.text-4xl LO ÚLTIMO
                 .article.flex-col.items-center.justify-center(v-for="article in $static.allStrapiArticle.edges" :key="article.node.id")
-                    g-image.object-contain.max-w-screen-md(:src="article.node.image.url" :alt="article.node.title")
+                    g-image.object-contain.sm_max-w-screen-md(:src="article.node.image.url" :alt="article.node.title")
                     h3.text-ui-gray.text-2xl.font-bold {{ article.node.title }}
                     p.text-ui-navy.text-sm.font-bold RECETAS
-                    p.text-ui-typo.text-base(class="w-3/4") #[VueShowdown(:markdown="article.node.content | truncate(110,' ...')" flavor="github" :options="{ emoji: true }")]
+                    p.text-ui-typo.text-base(class="sm_w-3/4") #[VueShowdown(:markdown="article.node.content | truncate(110,' ...')" flavor="github" :options="{ emoji: true }")]
                     a(:href="`/blog/${article.node.slug}`").text-ui-navy.text-base.font-bold Leer más >
                     p.text-ui-typo.text-xs.font-bold {{ article.node.published_at }}
-                    .border.boder-ui-typo.mt-5(class="w-3/4")
+                    .border.boder-ui-typo.mt-5(class="sm_w-3/4")
             LayoutSidebar
 
 </template>
