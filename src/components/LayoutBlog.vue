35a0c7c5d50293844b58cbef239f6da73e618c7b
LeslyePO <79282745+LeslyePO@users.noreply.github.com>
2023-05-23 15:30:40 -0600

    Update LayoutBlog.vue

--- src/components/LayoutBlog.vue
+++ src/components/LayoutBlog.vue
@@ -1,1 +1,25 @@
+<template lang="pug">
+    section.blog
+        .flex.sm_flex-row.flex-col.justify-center.items-start.my-10
+            .flex.flex-col.justify-start.sm_ml-5.mx-3(class="sm_w-2/3")
+                h2.text-ui-gray.text-4xl LO ÚLTIMO
+                
 
+</template>
+
+<script>
+import LayoutSidebar from '@/components/LayoutSidebar'
+export default {
+    components:{
+        LayoutSidebar
+    },
+    filters:{
+        truncate: function (text, length, suffix) {
+            return text.substring(0, length) + suffix;
+        },
+    },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
